37b1fd19a19b675b97be28b6f152620aceb8a093
Indrajeet_kedari <indrajeetkedari421@gmail.com>
2024-08-29 17:17:00 +0530

    added all the missing annotations in the general component controller

--- src/main/java/com/qdtas/controller/GeneralInformationController.java
+++ src/main/java/com/qdtas/controller/GeneralInformationController.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -16,7 +19,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/generalinfo")
 @Tag(name = "10. General_Information")
-@CrossOrigin(origins = "*")
+@CrossOrigin
 public class GeneralInformationController {
 
     @Autowired
